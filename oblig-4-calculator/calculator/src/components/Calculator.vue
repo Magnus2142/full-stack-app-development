a9edca6c956861a1f925a8d0853c64ed99b4c322
magnus2142 <magnus.bredeli@hotmail.com>
2021-04-07 14:02:17 +0200

    Created a simple weather app by following a youtube tutorial

--- oblig-4-calculator/calculator/src/components/Calculator.vue
+++ oblig-4-calculator/calculator/src/components/Calculator.vue
@@ -1,27 +1,28 @@
 <template>
-  <div class="calculator">
-    <div class="display">{{current || "0"}}</div>
-    <div @click="clear" class="btn">C</div>
-    <div @click="sign" class="btn">+/-</div>
-    <div @click="percent" class="btn">%</div>
-    <div @click="divide" class="btn operator">/</div>
-    <div @click="append('7')" class="btn">7</div>
-    <div @click="append('8')" class="btn">8</div>
-    <div @click="append('9')" class="btn">9</div>
-    <div @click="times" class="btn operator">x</div>
-    <div @click="append('4')" class="btn">4</div>
-    <div @click="append('5')" class="btn">5</div>
-    <div @click="append('6')" class="btn">6</div>
-    <div @click="minus" class="btn operator">-</div>
-    <div @click="append('1')" class="btn">1</div>
-    <div @click="append('2')" class="btn">2</div>
-    <div @click="append('3')" class="btn">3</div>
-    <div @click="plus" class="btn operator">+</div>
-    <div @click="append('0')" class="btn zero">0</div>
-    <div @click="dot" class="btn">.</div>
-    <div @click="equal" class="btn operator">=</div>
-  
-
+  <div>
+      <div class="calculator">
+      <div class="display">{{current || "0"}}</div>
+      <div @click="clear" class="btn">C</div>
+      <div @click="sign" class="btn">+/-</div>
+      <div @click="percent" class="btn">%</div>
+      <div @click="divide" class="btn operator">/</div>
+      <div @click="append('7')" class="btn">7</div>
+      <div @click="append('8')" class="btn">8</div>
+      <div @click="append('9')" class="btn">9</div>
+      <div @click="times" class="btn operator">x</div>
+      <div @click="append('4')" class="btn">4</div>
+      <div @click="append('5')" class="btn">5</div>
+      <div @click="append('6')" class="btn">6</div>
+      <div @click="minus" class="btn operator">-</div>
+      <div @click="append('1')" class="btn">1</div>
+      <div @click="append('2')" class="btn">2</div>
+      <div @click="append('3')" class="btn">3</div>
+      <div @click="plus" class="btn operator">+</div>
+      <div @click="append('0')" class="btn zero">0</div>
+      <div @click="dot" class="btn">.</div>
+      <div @click="equal" class="btn operator">=</div>  
+    </div>  
+    <textarea id="log" cols="30" rows="10"></textarea>
   </div>
 </template>
 
@@ -33,6 +34,7 @@ export default {
       current: "",
       operator: null,
       operatorClicked: false,
+      log: "",
     }
   },
   methods: {
@@ -80,6 +82,7 @@ export default {
     equal(){
       this.current = `${this.operator(parseFloat(this.previous), parseFloat(this.current))}`;
       this.previous = null;
+      document.getElementById('log').innerHTML += this.operator;
     }
   }
 
